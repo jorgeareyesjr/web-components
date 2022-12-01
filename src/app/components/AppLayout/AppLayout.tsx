d25d931e3b87084db61f58a021ea65f235267308
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2022-12-01 00:10:39 -0800

    Update the main app's AppLayout base component.

---
+++ src/app/components/AppLayout/AppLayout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/* Components */
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import './AppLayout.css'
+
+export default function AppLayout() {
+	return (
+		<>
+			<Header />
+			<Main />
+		</>
+	);
+};
