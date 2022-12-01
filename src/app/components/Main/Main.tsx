a8bf7b4d359ad131acb0e3fa68035c97c738fdd2
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2022-12-01 00:20:28 -0800

    Update the app's Main Component - dynamically set the css variables responsible for transforming/translating the nav along the X and Y axis.

--- src/app/components/Main/Main.tsx
+++ src/app/components/Main/Main.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
+import { getNavDisplaySizeSelector } from '../../_redux/nav/selectors';
+import './Main.css';
 
-export default function Main() {
+export default function Main () {
+	// Get the app's main nav size from redux store and pass it as a variable to calculate how much the Main component should transform/translate along the X and Y axis.
+	const navDisplaySize = useSelector(getNavDisplaySizeSelector);
+	const styles= {
+		color: 'inherit',
+		'--transformX': `translateX(${navDisplaySize.width}px)`,
+		'--negative-transformX': `translateX(${-(navDisplaySize.width)}px)`,
+		'--transformY': `translateY(${navDisplaySize.height}px)`,
+		'--negative-transformY': `translateY(${-(navDisplaySize.height)}px)`,
+	};
+
 	return (
-		<main className={`c-main`}>
+		<main className={`c-main`} style={styles}>
 			{/* An <Outlet> renders whatever child route is currently active,
 			so you can think about this <Outlet> as a placeholder for
-			the a defined child route. */}
+			the child routes we defined above. */}
 			<Outlet />
 		</main>
 	);
