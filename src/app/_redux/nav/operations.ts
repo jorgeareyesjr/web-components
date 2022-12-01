1d52e2dce603919b68bd07955a0036b1e07fa357
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2022-12-01 00:18:12 -0800

    Update redux support for managing app nav state - give it the ability to set and get the nav component's size parameters.

--- src/app/_redux/nav/operations.ts
+++ src/app/_redux/nav/operations.ts
@@ -1,9 +1,13 @@
 import {
 	toggleNavDisplay,
 	closeNavDisplay,
+	getNavDisplaySize,
+	setNavDisplaySize,
 } from './actions';
 
 export {
 	toggleNavDisplay,
 	closeNavDisplay,
-}
+	getNavDisplaySize,
+	setNavDisplaySize,
+};
--- src/app/_redux/nav/reducer.ts
+++ src/app/_redux/nav/reducer.ts
@@ -1,18 +1,29 @@
+import { combineReducers } from 'redux';
 import {
 	TOGGLE_NAV_DISPLAY,
 	CLOSE_NAV_DISPLAY,
+	SET_NAV_DISPLAY_SIZE,
 } from './actionTypes';
-import { combineReducers } from 'redux';
 
-const initialUIState = {
+// TODO: Add types.ts
+
+const initialUIState: any = {
 	navIsOpen: false,
 };
 
-const initialState ={
-	ui: initialUIState,
+const initialDataState: any = {
+	navDisplaySize: {
+		width: undefined,
+		height: undefined,
+	},
 };
 
-const uiReducer = (state = initialState.ui, action: { type: any; }) => {
+const initialState: any = {
+	ui: initialUIState,
+	data: initialDataState,
+};
+
+const uiReducer = (state = initialState.ui, action: any) => {
 	switch (action.type) {
 		case TOGGLE_NAV_DISPLAY:
 			return {
@@ -28,9 +39,26 @@ const uiReducer = (state = initialState.ui, action: { type: any; }) => {
 			return {
 				...state,
 			};
-	}
+	};
 };
 
-const themesReducer = combineReducers({ ui: uiReducer });
+const dataReducer = (state = initialState.data, action: any) => {
+	switch (action.type) {
+		case SET_NAV_DISPLAY_SIZE:
+			return {
+				...state,
+				navDisplaySize: action.payload,
+			};
+		default:
+			return {
+				...state,
+			};
+	};
+};
 
-export default themesReducer;
+const navReducer = combineReducers({
+	ui: uiReducer,
+	data: dataReducer,
+});
+
+export default navReducer;
