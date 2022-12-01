ffae67525d070e33a78033e0c5811169292b6902
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2022-12-01 00:27:19 -0800

    Create new test pages.

---
+++ src/app/pages/Landing/Landing.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Spinner from '../../components/Spinner/Spinner';
+
+export default function Landing() {
+	return (
+		<>
+			<Spinner message={`Test Landing Page`} />
+		</>
+	);
+};
---
+++ src/app/pages/Overview/Overview.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Spinner from '../../components/Spinner/Spinner';
+
+export default function Overview() {
+	return (
+		<>
+			<Spinner message={`Test Overview Page`} />
+		</>
+	);
+};
