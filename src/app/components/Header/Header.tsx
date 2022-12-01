bc37db971a66d3dff85e14d6830511f0338c92ed
Jorge A. Reyes Jr <reyes.junior.27@gmail.com>
2022-12-01 00:18:52 -0800

    Clean up the main app Header component.

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,37 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { toggleNavDisplay } from '../../_redux/nav/actions';
-import { AppRoutes } from '../Router/Routes';
+
+/* Redux */
+import { toggleNavDisplay } from '../../_redux/nav/operations';
+
+/* Components */
+import Nav from '../Nav/Nav';
+import './Header.css';
 
 export default function Header() {
 	const dispatch = useDispatch();
 	const toggleNav = () => dispatch(toggleNavDisplay());
-
-	const mapLinks = () => {
-		return AppRoutes.map((route, i) => (
-			<li key={i} className={`c-list__listing`}>
-				<Link
-					to={route.path}
-					className={`c-link`}
-					onClick={toggleNav}
-				>
-					{route.name}
-				</Link>
-			</li>
-		));
-	};
-
 	return (
 		<header className={`c-header`}>
 			<button className={`c-button`} onClick={toggleNav}>
-				Toggle Nav
+				<span className={`c-button__span`}></span>
+				<span className={`c-button__span`}></span>
+				<span className={`c-button__span`}></span>
 			</button>
-			<nav className={`c-nav c-nav--primary`}>
-				<ul className={`c-list`}>
-					{ mapLinks()}
-				</ul>
-			</nav>
+			<Nav />
 		</header>
 	);
 };
